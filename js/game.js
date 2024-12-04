842e912f3a3be3c9db85f5ae90c7daef3ff57873
Tiago-2007 <tiago.machado.3105@gmail.com>
2024-12-04 09:11:07 +0000

    player animation

--- js/game.js
+++ js/game.js
@@ -2,24 +2,34 @@ class Game {
     constructor(canvas, context) {
         this.canvas = canvas;
         this.ctx = context;
-        this.widht = this.canvas.widht;
+        this.width = this.canvas.width;
         this.height = this.canvas.height;
         this.player = new Player(this);
     }
 
     render() {
         this.ctx.fillStyle = 'red';
+        this.player.update();
         this.player.draw();
-     }
+    }
 }
 
 window.addEventListener('load', function() {
     const canvas = document.getElementById('game-layout');
     const ctx = canvas.getContext('2d');
-    canvas.widht = 720;
+    canvas.width = 720;
     canvas.height = 720;
 
     const game = new Game(canvas, ctx);
-    game.render();
-});
+    
+    function animate() {  
+    
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+    
+        game.render();
+    this.requestAnimationFrame(animate);
+    }  
 
+    this.requestAnimationFrame(animate);
+
+});
--- js/player.js
+++ js/player.js
@@ -6,10 +6,16 @@ class Player {
         this.widht = 200;
         this.height = 200;
     }
-
+    
     draw() {
         this.game.ctx.fillRect(this.x, this.y, this.widht, this.height);
     }
+
+
+    update() {
+        this.x++;    
+    }
 }
+
 
 
